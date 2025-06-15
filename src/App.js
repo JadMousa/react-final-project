cee2cf9dc4a9da4ab7353436601f71b6be5f3177
JadMousa <jadmousa12@gmail.com>
2025-06-15 21:07:08 +0300

    Fixing Books API and Navbar

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import './App.css';
 import Books from './pages/books';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import NavigationBar from './components/navigationbar';
 
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-         
-         <Route path="/" element={<Home />} />
-        <Route path="/books" element={<Books />} />
-        {/* Add other routes here */}
-      </Routes>
-    </Router>
-  );
+    return (
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
+    );
 }
 
 export default App;

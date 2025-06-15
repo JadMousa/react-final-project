c027faac9d638e94380cc78e238696af96b2aaaa
JadMousa <jadmousa12@gmail.com>
2025-06-15 20:50:29 +0300

    Successfuly Created home route

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// src/pages/Home.js
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <Container className="my-5 text-center">
+      <h1>📚 Welcome to the Library App</h1>
+      <p>Browse our collection or search for books using the menu.</p>
+    </Container>
+  );
+}
+
+export default Home;

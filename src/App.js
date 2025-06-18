c6b1e1faa829d6e379c0b799c53a72b0771ba5bf
JadMousa <jadmousa12@gmail.com>
2025-06-18 19:50:41 +0300

    Fixing mistakes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavigationBar from './components/navigationbar';
 import Footer from './components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginButton from './components/loginbutton';
-import LogoutButton from './components/logoutbutton';
-import UserProfile from './components/userprofile.js';
 import Account from './pages/account';
 import AddBookPage from './pages/AddBookPage';
 import MyBooks from './pages/mybooks';
 import Books from './pages/books';
 import BookDetails from './pages/BookDetails';
+import Home from './pages/home';
 
 function App() {
   return (
@@ -18,6 +16,7 @@ function App() {
       <NavigationBar />
       <Routes>
         {/* No Home Route */}
+        <Route path="/" element={<Home />} />
         <Route path="/books" element={<Books />} />
         <Route path="/books/:id" element={<BookDetails />} />
         <Route path="/account" element={<Account />} />
